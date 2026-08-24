3a063545cb4abd20895e065cabfdf6f7b1baf453
Azerty <quentin.guillermin@orange.fr>
2026-08-24 21:44:18 +0200

    renamed variables

--- Turing Machine/parser.js
+++ Turing Machine/parser.js
@@ -1,25 +1,25 @@
 "use strict";
-function parseState(stateRules) {
+function parseState(stateCode) {
     const state = (i) => parseInt(i);
     const move = (i) => ({R: 1, L: -1}[i] ?? 0);
     const symbol = (i) => i.charCodeAt(0) - 65;
 
     const parsed = [];
-    for (let i = 0; i < stateRules.length; i += 3) {
-        const isUndefined = stateRules.slice(i, i + 3).includes("-");
+    for (let i = 0; i < stateCode.length; i += 3) {
+        const isUndefined = stateCode.slice(i, i + 3).includes("-");
 
         parsed.push(isUndefined ? [] : [
-            state(stateRules[i]),
-            move(stateRules[i + 1]),
-            symbol(stateRules[i + 2]),
+            state(stateCode[i]),
+            move(stateCode[i + 1]),
+            symbol(stateCode[i + 2]),
         ]);
     }
     return parsed;
 }
 
-export function parse(ruleset) {
+export function parse(code) {
     const parsed = [];
-    const statesRules = ruleset.split("_");
+    const statesRules = code.split("_");
 
     for (let i = 0; i < statesRules.length; i++) {
         parsed.push(parseState(statesRules[i]))
@@ -27,12 +27,12 @@ export function parse(ruleset) {
     return parsed;
 }
 
-export function unparse(ruleset) {
+export function unparse(code) {
     const state = (i) => i.toString();
     const move = (i) => new Map([[-1, "L"], [1, "R"]]).get(i) ?? "I";
     const symbol = (i) => String.fromCharCode(i + 65);
 
-    return ruleset.map((st) =>
+    return code.map((st) =>
         st.map((sy) =>
             sy.length < 3
             ? "---"
