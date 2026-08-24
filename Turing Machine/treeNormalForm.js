6cb8e84440372666ed54cf0a06dc928f1a1c1452
Azerty <quentin.guillermin@orange.fr>
2026-08-24 21:43:18 +0200

    added comments

--- Turing Machine/treeNormalForm.js
+++ Turing Machine/treeNormalForm.js
@@ -2,11 +2,15 @@
 import {newMachine} from "./runner.js";
 
 export function enumerate(maxStates, maxSymbols, maxSteps) {
-    function* nextTransition(code, instructions) {
+    function* nextTransition(code, instructions, recState, recSymbol) {
+        // Run the machine until an undefined transition
         const machine = newMachine(code, maxSteps);
         const steps = machine.run();
-        const {state, symbol} = machine.getData();
 
+        const state = machine.getData().state;
+        const symbol = machine.readCell();
+
+        // Check if the machine is nonhalting
         if (steps < 0) {
             yield code;
             return;
@@ -14,17 +18,27 @@ export function enumerate(maxStates, maxSymbols, maxSteps) {
 
         const instruction = code?.[state]?.[symbol];
         if (!instruction || instruction.length < 3) {
+            // Check if the code is full
             instructions++;
             if (instructions >= maxStates * maxSymbols) {
                 yield code;
                 return;
             }
 
-            for (let nextState = 0; nextState < maxStates; nextState++) {
+            // Enumerate every possible canditates
+            const maxNextState = Math.min(maxStates - 1, recState + 1);
+            const maxWrite = Math.min(maxSymbols - 1, recSymbol + 1);
+
+            for (let nextState = 0; nextState <= maxNextState; nextState++) {
                 for (let move = -1; move <= 1; move += 2) {
-                    for (let write = 0; write < maxSymbols; write++) {
+                    for (let write = 0; write <= maxWrite; write++) {
                         code[state][symbol] = [write, move, nextState];                        
-                        yield* nextTransition(code, instructions);
+                        yield* nextTransition(
+                            code,
+                            instructions,
+                            Math.max(recState, nextState),
+                            Math.max(recSymbol, write)
+                        );
                         code[state][symbol] = [];
                     }
                 }
@@ -45,5 +59,5 @@ export function enumerate(maxStates, maxSymbols, maxSteps) {
         return code;
     }
 
-    return nextTransition(emptyCode(), 1);
+    return nextTransition(emptyCode(), 1, 1, 1);
 }
