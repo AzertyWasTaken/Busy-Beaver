c2aa02a54659f068887379ebb29e55854c0a794f
Azerty <quentin.guillermin@orange.fr>
2026-08-24 21:45:15 +0200

    added comments

--- Turing Machine/enumerator.js
+++ Turing Machine/enumerator.js
@@ -1,11 +1,13 @@
 "use strict";
 export function enumerate(maxStates, maxSymbols) {
     function* nextTransition(currState, currSymbol, code, stateCode, hasHalt) {
+        // Check if the code is full
         if (currState >= maxStates) {
             if (hasHalt) yield code;
             return;
         }
 
+        // Check if the state code is full
         if (currSymbol >= maxSymbols) {
             code.push(stateCode);
             yield* nextTransition(currState + 1, 0, code, [], hasHalt);
@@ -13,6 +15,7 @@ export function enumerate(maxStates, maxSymbols) {
             return;
         }
 
+        // Enumerate every possible canditates
         for (let nextState = 0; nextState < maxStates; nextState++) {
             for (let move = -1; move <= 1; move += 2) {
                 for (let write = 0; write < maxSymbols; write++) {
@@ -23,6 +26,7 @@ export function enumerate(maxStates, maxSymbols) {
             }
         }
 
+        // Check if an halting transition can be added
         if (!hasHalt) {
             stateCode.push([]);
             yield* nextTransition(currState, currSymbol + 1, code, stateCode, true);
